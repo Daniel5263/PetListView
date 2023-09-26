fdf76350013b0cd547b28680e3d4cf7c8b44c03d
Daniel5263 <dany9000@gmail.com>
2023-09-26 12:59:31 -0500

    Added model Pet Add member data Add string resource file arrays.xml Add ListView

--- app/src/androidTest/java/model/Pet.java
+++ app/src/androidTest/java/model/Pet.java
@@ -2,5 +2,37 @@ package model;
 
 public class Pet {
 
+    String breed, name;
+    int age;
+
+    public Pet(String thisName, String thisBreed, int thisAge) {
+        name = thisName;
+        breed = thisBreed;
+        age = thisAge;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 }
